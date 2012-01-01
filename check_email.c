6fc735f5de222b9602d059650a486d194cdbfe30
Jose Ricardo Ziviani <jrziviani@gmail.com>
2012-01-01 13:02:02 -0200

    Added README and improved the code

--- check_email.c
+++ check_email.c
@@ -14,17 +14,21 @@
 
 #include "check_email.h"
 
+// flush remaining response data from the smtp server
 static void socket_flush(int *socket)
 {
     char buffer[1024];
     int len = 0;
 
     do {
+
+        // read response data while buffer is fully filled out
         len = read(*socket, buffer, 1023);
+
     } while (len == 1023);
 }
 
-
+// send a message to the server
 static int send_message(int *socket, char *msg)
 {
     int len = 0;
@@ -32,18 +36,25 @@ static int send_message(int *socket, char *msg)
     
     write(*socket, msg, strlen(msg));
 
+    // wait 1s before reading the response back from the server
     sleep(1);
 
+    // read the first three bytes, which is the response code we need to know
+    // if the command sent was successfully processed
     if ((len = read(*socket, buffer, 3)) <= 0)
-        return 1;
+        return SOCKET_READ_ERROR;
 
     buffer[3] = '\0';
 
+    // cleans remaining data
     socket_flush(socket);
 
+    // return a integer representation of the smtp returning code
     return atoi(buffer);
 }
 
+// open a socket to the smtp server found and check whether it is a valid email 
+// or not
 static int smtp_query(char *server, char *email)
 {
     struct hostent *host;
@@ -55,11 +66,11 @@ static int smtp_query(char *server, char *email)
 
     // create a new socket
     if ((socket_h = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-        return 1;
+        return SOCKET_CREATE_ERROR;
 
     // resolve the host
     if ((host = gethostbyname(server)) == NULL)
-        return 2;
+        return SOCKET_HOSTBYNAME_ERROR;
     
     // setup family (internet), SMTP port (25) and the address
     server_addr.sin_family      = AF_INET;
@@ -68,24 +79,23 @@ static int smtp_query(char *server, char *email)
 
     // connect to the server
     if (connect(socket_h, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1)
-        return 3;
+        return SOCKET_CONNECT_ERROR;
 
     // init SMTP connection 
     if (send_message(&socket_h, "HELO check_email\r\n") != 220)
-        return 4;
+        return SMTP_HELO_ERROR;
 
     // tell who wants to send the message, this step is mandatory before RCPT
     // TO but it is not important for us here
     if (send_message(&socket_h, "MAIL FROM:<check_email@invalid.com>\r\n") != 250)
-        return 5;
+        return SMTP_MAILFROM_ERROR;
 
     char buffer[256];
-    //snprint append /0 into buffer
     snprintf(buffer, 255, "RCPT TO:<%s>\r\n", email);
     
     // check if the server recognizes this email address as valid
     if (send_message(&socket_h, buffer) != 250)
-        return 6;
+        return SMTP_INVALID_EMAIL_ADDR;
 
     // close the smtp connection
     send_message(&socket_h, "QUIT\r\n"); 
@@ -93,9 +103,11 @@ static int smtp_query(char *server, char *email)
     // close the socket
     close(socket_h);
 
-    return 0;
+    return SUCCESS;
 }
 
+// based on a e-mail address domain, look up the dns mx (mail exchange)
+// record in order to find how it should be routed 
 static int dns_mx_lookup(char *domain, char ***server_list)
 {
     int answer_len, message_number;
@@ -105,23 +117,23 @@ static int dns_mx_lookup(char *domain, char ***server_list)
     ns_rr ns_record;
     ns_msg ns_handler;
 
-    // lookup the DNS MX records - responsible for accepting email messages    
+    // lookup the DNS MX records - responsible for routing email messages    
     if ((answer_len = res_query(domain, C_IN, T_MX, answer, NS_PACKETSZ)) < 0)
-        return -1;
+        return DNS_RESQUERY_ERROR;
 
     // initparser is the first function to be called when using the name server
     // library routines. It fills in the ns_handler data structure
     if (ns_initparse(answer, answer_len, &ns_handler) < 0)
-        return -2;
+        return DNS_INITPARSER_ERROR;
 
     // get the counter from the header section of the response message
     if ((message_number = ns_msg_count(ns_handler, ns_s_an)) < 0)
-        return -3;
+        return DNS_MSGCOUNT_ERROR;
 
     // create a list of strings for each of all servers found
     response = (char**)malloc(sizeof(char*) * message_number);
     if (response == NULL)
-        return -4;
+        return OUT_OF_MEMORY;
 
     int i;
     for (i = message_number; i >= 0; --i) {
@@ -143,12 +155,12 @@ static int dns_mx_lookup(char *domain, char ***server_list)
             // (message_number to 0) so I need to clean up from the not
             // allocated index + 1 up to the message_number
             int remaining_alloc = i + 1;
-            for (remaining_alloc; remaining_alloc < message_number; ++remaining_alloc)
+            for (; remaining_alloc < message_number; ++remaining_alloc)
                 free(response[remaining_alloc]);
 
             // free the strings list
             free(response);
-            return -5;
+            return OUT_OF_MEMORY;
         }
 
         // extract the name of the mail server found into response[i]
@@ -157,7 +169,7 @@ static int dns_mx_lookup(char *domain, char ***server_list)
                                ns_rr_rdata(ns_record) + NS_INT16SZ,
                                response[i],
                                MAXDNAME) < 0) {
-            return -6;
+            return DNS_UNCOMPRESS_ERROR;
         }
     }
     
@@ -168,55 +180,54 @@ static int dns_mx_lookup(char *domain, char ***server_list)
     return message_number;
 }
 
-/*
-http://docstore.mik.ua/orelly/networking_2ndEd/dns/ch15_02.htm
-http://stackoverflow.com/questions/1688432/querying-mx-record-in-c-linux/1689539#1689539
-http://www.linuxforums.org/articles/e-mail-architecture-part-ii_278.html
-
-gcc -g check_email.c -lresolv -o check
-
-valgrind -v --leak-check=full ./check
-*/
-int main()
+int main(int argc, char *argv[])
 {
     char **list = NULL;
-    char *email = "test@gmail.com.br";
+    char *email;
+
+    if (argc != 2) {
+        printf("%d\n", INPUT_INVALID);
+        return INPUT_INVALID;
+    }
+
+    email = argv[1];
 
     char* pdomain = strchr(email, '@');
     if (!pdomain || !++pdomain)
-        return -1;
+        return INPUT_INVALID_EMAIL;
 
     if (strlen(pdomain) >= 512)
-        return 1;
+        return OUT_OF_BOUND;
 
-    int size = dns_mx_lookup(pdomain, &list);
+    int msgs_number = dns_mx_lookup(pdomain, &list);
 
     // negative response = error happened
-    if (size < 0)
-        return 1;
+    if (msgs_number < SUCCESS)
+        return msgs_number;
 
     // no server found - the mail server for the specified domain is not valid
-    if (size == 0 || list == NULL)
-        return 0;
+    if (msgs_number == 0 || list == NULL)
+        return INPUT_INVALID_DOMAIN;
 
     int i;
-    for (i = 0; i < size; ++i) {
+    int ret;
+    for (i = 0; i < msgs_number; ++i) {
 
         printf("%d - %s\n", i, list[i]);
 
-        int ret = smtp_query(list[i], email);
+        ret = smtp_query(list[i], email);
 
-        if (ret == 6) {
+        if (ret != SUCCESS) {
             printf("This email is not valid\n");
             break;
-        } else if (ret == 0) {
+        } else {
             printf("This is a valid e-mail\n");
             break;
         }
     }
 
     // free allocated resources
-    for (i = 0; i < size; ++i) {
+    for (i = 0; i < msgs_number; ++i) {
 
         // item by item...
         free(list[i]);
@@ -226,6 +237,6 @@ int main()
     free(list);
     list = NULL;
 
-    return 0;
+    return ret;
 }
 
--- check_email.h
+++ check_email.h
@@ -1,6 +1,32 @@
 #ifndef CHECKEMAIL_H_
 #define CHECKEMAIL_H_
 
+enum rcode {
+
+    SUCCESS                  = 0,
+    
+    SOCKET_READ_ERROR        = -1,
+    SOCKET_CREATE_ERROR      = -2,
+    SOCKET_HOSTBYNAME_ERROR  = -3,
+    SOCKET_CONNECT_ERROR     = -4,
+
+    SMTP_HELO_ERROR          = -5,
+    SMTP_MAILFROM_ERROR      = -6,
+    SMTP_INVALID_EMAIL_ADDR  = -7,
+
+    DNS_RESQUERY_ERROR       = -8,
+    DNS_INITPARSER_ERROR     = -9,
+    DNS_MSGCOUNT_ERROR       = -10,
+    DNS_UNCOMPRESS_ERROR     = -11,
+
+    INPUT_INVALID            = -12,
+    INPUT_INVALID_EMAIL      = -13,
+    INPUT_INVALID_DOMAIN     = -14,
+
+    OUT_OF_BOUND             = -15,
+    OUT_OF_MEMORY            = -16
+};
+
 static int dns_mx_lookup(char *domain, char ***server_list);
 
 #endif
